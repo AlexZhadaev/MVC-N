81c033f5caa85cda6e044e040686ad58fecbe6f8
AlexZhadaev <pvt.vatnik@gmail.com>
2021-05-26 00:06:54 +0300

    project created

---
+++ MVC-N/MVC-N/Comments/Controller/CommentsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  MVC-N
+//
+//  Created by Жадаев Алексей on 25.05.2021.
+//
+
+import UIKit
+
+class CommentsViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+}
+
+extension CommentsViewController: UITableViewDelegate {
+    
+}
+
+extension CommentsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+ 
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+ 
+    }
+    
+    
+}
